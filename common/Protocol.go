323444ccaaa5048082a132891b2adb14cf20ac50
lincongcong <lincongcong@tangbull.com>
2019-08-13 20:23:36 +0800

    clean up

--- common/Protocol.go
+++ common/Protocol.go
@@ -12,7 +12,7 @@ type Job struct {
 	CronExpr string `json:"cronExpr"`
 }
 
-//事件抽象 , worker的JobMgr和Scheduler之间的协议
+//事件抽象 , worker的JobWatcher和Scheduler之间的协商
 type JobEvent struct {
 	EventType int
 	Job       *Job
@@ -23,36 +23,6 @@ type Response struct {
 	Errno int         `json:"errno"`
 	Msg   string      `json:"msg"`
 	Data  interface{} `json:"data"`
-}
-
-// 任务执行日志
-type JobLog struct {
-	JobName      string `json:"jobName" bson:"jobName"`           // 任务名字
-	Command      string `json:"command" bson:"command"`           // 脚本命令
-	Err          string `json:"err" bson:"err"`                   // 错误原因
-	Output       string `json:"output" bson:"output"`             // 脚本输出
-	PlanTime     int64  `json:"planTime" bson:"planTime"`         // 计划开始时间
-	ScheduleTime int64  `json:"scheduleTime" bson:"scheduleTime"` // 实际调度时间
-	StartTime    int64  `json:"startTime" bson:"startTime"`       // 任务执行开始时间
-	EndTime      int64  `json:"endTime" bson:"endTime"`           // 任务执行结束时间
-}
-
-type LogBatch struct {
-	Logs []interface{} //为什么要用interface{}
-}
-
-// 任务日志过滤条件
-type JobLogFilter struct {
-	JobName string `bson:"jobName"`
-}
-
-// 任务日志排序规则
-type SortLogByStartTime struct {
-	SortOrder int `bson:"startTime"` // {startTime: -1}
-}
-
-func ExtractWorkerIP(regKey string) string {
-	return strings.TrimPrefix(regKey, JOB_WORKER_DIR)
 }
 
 // 应答方法
@@ -69,6 +39,10 @@ func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err er
 	// 2, 序列化json
 	resp, err = json.Marshal(response)
 	return
+}
+
+func ExtractWorkerIP(regKey string) string {
+	return strings.TrimPrefix(regKey, JOB_WORKER_DIR)
 }
 
 func UnpackJob(value []byte) (ret *Job, err error) {
